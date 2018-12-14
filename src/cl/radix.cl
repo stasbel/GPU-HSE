4192ede2fa4fac4b8de243b64e19801f0374be5a
Stanislav Belyaev <stasbelyaev96@gmail.com>
2018-12-14 22:42:24 +0300

    feat(radix): Implement Radix Sort

--- src/cl/radix.cl
+++ src/cl/radix.cl
@@ -1,4 +1,76 @@
-__kernel void radix(__global unsigned int* as)
-{
-    // TODO
+#ifdef __CLION_IDE__
+
+#include <libgpu/opencl/cl/clion_defines.cl>
+
+#endif
+
+#define WORK_GROUP_SIZE 256
+
+#line 10
+
+__kernel void count(
+        __global unsigned int *as, __global unsigned int *cs,
+        unsigned int n, unsigned int mask_offset, unsigned int mask_width
+) {
+    unsigned int gid = get_global_id(0), mask = ((1UL << mask_width) - 1) << mask_offset, n_vals = 1 << mask_width;
+
+    if (gid < n) {
+        for (int i = 0; i < n_vals; i++)
+            cs[i * n + gid] = ((as[gid] & mask) >> mask_offset) == i ? 1 : 0;
+    }
+}
+
+__kernel void scan(
+        __global unsigned int *as, __global unsigned int *bs, __global unsigned int *cs,
+        unsigned int n, int zero_bs
+) {
+    unsigned int gid = get_global_id(0), lid = get_local_id(0), block_size = get_local_size(0), gr_id = get_group_id(0);
+
+    __local unsigned int local_a[WORK_GROUP_SIZE], local_b[WORK_GROUP_SIZE];
+    __local unsigned int *a = local_a, *b = local_b;
+
+    if (gid < n) {
+        a[lid] = as[gid];
+
+        if (lid == 0) {
+            if (!zero_bs)
+                a[0] += bs[gr_id];
+        }
+    } else {
+        a[lid] = 0;
+    }
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    for (int s = 1; s < block_size; s <<= 1) {
+        if (lid > (s - 1)) {
+            b[lid] = a[lid] + a[lid - s];
+        } else {
+            b[lid] = a[lid];
+        }
+
+        barrier(CLK_LOCAL_MEM_FENCE);
+
+        __local unsigned int *tmp = a;
+        a = b;
+        b = tmp;
+    }
+
+    if (gid < n) {
+        cs[gid] = a[lid];
+
+        if (lid == 0)
+            bs[gr_id + 1] = a[block_size - 1];
+    }
+}
+
+__kernel void reorder(
+        __global unsigned int *as, __global unsigned int *os, __global unsigned int *bs,
+        unsigned int n, unsigned int mask_offset, unsigned int mask_width
+) {
+    unsigned int gid = get_global_id(0), mask = ((1UL << mask_width) - 1) << mask_offset, n_vals = 1 << mask_width;
+
+    if (gid < n) {
+        bs[os[((as[gid] & mask) >> mask_offset) * n + gid] - 1] = as[gid];
+    }
 }
--- src/main_radix.cpp
+++ src/main_radix.cpp
@@ -4,7 +4,6 @@
 #include <libgpu/context.h>
 #include <libgpu/shared_device_buffer.h>
 
-// Этот файл будет сгенерирован автоматически в момент сборки - см. convertIntoHeader в CMakeLists.txt:18
 #include "cl/radix_cl.h"
 
 #include <vector>
@@ -13,8 +12,7 @@
 
 
 template<typename T>
-void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line)
-{
+void raiseFail(const T &a, const T &b, const std::string &message, std::string filename, int line) {
     if (a != b) {
         std::cerr << message << " But " << a << " != " << b << ", " << filename << ":" << line << std::endl;
         throw std::runtime_error(message);
@@ -23,15 +21,36 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
 
+gpu::gpu_mem_32u prefixSum(ocl::Kernel &scan, gpu::gpu_mem_32u &as_gpu, unsigned int n, unsigned int wg_size) {
+    unsigned int res_n = (n + wg_size - 1) / wg_size * wg_size;
 
-int main(int argc, char **argv)
-{
+    gpu::gpu_mem_32u sums;
+    sums.resizeN(res_n);
+    gpu::gpu_mem_32u b_sums;
+    b_sums.resizeN(res_n / wg_size + 1);
+
+    unsigned int z = 0;
+    b_sums.writeN(&z, 1);
+
+    scan.exec(gpu::WorkSize(wg_size, n), as_gpu, b_sums, sums, n, 1);
+
+    if (wg_size >= n)
+        return sums;
+
+    b_sums = prefixSum(scan, b_sums, res_n / wg_size + 1, wg_size);
+    scan.exec(gpu::WorkSize(wg_size, n), as_gpu, b_sums, sums, n, 0);
+
+    return sums;
+}
+
+int main(int argc, char **argv) {
+    // Device
     gpu::Device device = gpu::chooseGPUDevice(argc, argv);
-
     gpu::Context context;
     context.init(device.device_id_opencl);
     context.activate();
 
+    // Data
     int benchmarkingIters = 10;
     unsigned int n = 32 * 1024 * 1024;
     std::vector<unsigned int> as(n, 0);
@@ -41,6 +60,7 @@ int main(int argc, char **argv)
     }
     std::cout << "Data generated for n=" << n << "!" << std::endl;
 
+    // CPU
     std::vector<unsigned int> cpu_sorted;
     {
         timer t;
@@ -50,38 +70,52 @@ int main(int argc, char **argv)
             t.nextLap();
         }
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "CPU: " << (n/1000/1000) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "CPU: " << double(n) / 1000 / 1000 / t.lapAvg() << " millions/s" << std::endl;
     }
-/*
+
+    // GPU
     gpu::gpu_mem_32u as_gpu;
     as_gpu.resizeN(n);
-
     {
-        ocl::Kernel radix(radix_kernel, radix_kernel_length, "radix");
-        radix.compile();
+        ocl::Kernel count(radix_kernel, radix_kernel_length, "count");
+        count.compile();
+        ocl::Kernel scan(radix_kernel, radix_kernel_length, "scan");
+        scan.compile();
+        ocl::Kernel reorder(radix_kernel, radix_kernel_length, "reorder");
+        reorder.compile();
+
+        unsigned int mask_width = 2, wg_size = 256, gw_size = n;
 
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
             as_gpu.writeN(as.data(), n);
 
-            t.restart(); // Запускаем секундомер после прогрузки данных чтобы замерять время работы кернела, а не трансфер данных
+            t.restart();
 
-            unsigned int workGroupSize = 128;
-            unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
-            radix.exec(gpu::WorkSize(workGroupSize, global_work_size),
-                       as_gpu, n);
+            gpu::gpu_mem_32u counts_gpu;
+            counts_gpu.resizeN(n * (1 << mask_width));
+            gpu::gpu_mem_32u bs_gpu;
+            bs_gpu.resizeN(n);
+
+            for (int i = 0; i < 32; i += mask_width) {
+                count.exec(gpu::WorkSize(wg_size, gw_size), as_gpu, counts_gpu, n, i, mask_width);
+                gpu::gpu_mem_32u s = prefixSum(scan, counts_gpu, n * (1 << mask_width), wg_size);
+                reorder.exec(gpu::WorkSize(wg_size, gw_size), as_gpu, s, bs_gpu, n, i, mask_width);
+                as_gpu.swap(bs_gpu);
+            }
+
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "GPU: " << (n/1000/1000) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "GPU: " << double(n) / 1000 / 1000 / t.lapAvg() << " millions/s" << std::endl;
 
         as_gpu.readN(as.data(), n);
     }
 
-    // Проверяем корректность результатов
+    // Test
     for (int i = 0; i < n; ++i) {
         EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
+
     return 0;
 }
