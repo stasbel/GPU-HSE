924eedbb2f130419b16c40f706b01e63a98d8960
Stanislav Belyaev <stasbelyaev96@gmail.com>
2018-12-14 22:41:59 +0300

    feat(bitonic): Implement Bitonic Sort

--- src/cl/bitonic.cl
+++ src/cl/bitonic.cl
@@ -1,4 +1,53 @@
-__kernel void bitonic(__global float* as)
-{
-    // TODO
+#ifdef __CLION_IDE__
+
+#include <libgpu/opencl/cl/clion_defines.cl>
+
+#endif
+
+#define WORK_GROUP_SIZE 256
+
+#line 10
+
+__kernel void bitonic_local(__global float *as, unsigned int cur_k, unsigned int k, unsigned int n) {
+    unsigned int global_id = get_global_id(0), local_id = get_local_id(0);
+
+    __local float mem[WORK_GROUP_SIZE];
+    if (global_id < n) {
+        mem[local_id] = as[global_id];
+    }
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    int is_sort_ascending = global_id % (2 * k) < k;
+    while (cur_k >= 1) {
+        if (global_id % (2 * cur_k) < cur_k && global_id + cur_k < n) {
+            float a = mem[local_id], b = mem[local_id + cur_k];
+
+            if ((a < b) != is_sort_ascending) {
+                mem[local_id] = b;
+                mem[local_id + cur_k] = a;
+            }
+        }
+
+        barrier(CLK_LOCAL_MEM_FENCE);
+        cur_k /= 2;
+    }
+
+    if (global_id < n) {
+        as[global_id] = mem[local_id];
+    }
+}
+
+
+__kernel void bitonic_initial(__global float *as, unsigned int cur_k, unsigned int k, unsigned int n) {
+    unsigned int global_id = get_global_id(0);
+
+    if (global_id % (2 * cur_k) < cur_k && global_id + cur_k < n) {
+        float a = as[global_id], b = as[global_id + cur_k];
+
+        if ((a < b) != (global_id % (2 * k) < k)) {
+            as[global_id] = b;
+            as[global_id + cur_k] = a;
+        }
+    }
 }
--- src/main_bitonic.cpp
+++ src/main_bitonic.cpp
@@ -4,7 +4,6 @@
 #include <libgpu/context.h>
 #include <libgpu/shared_device_buffer.h>
 
-// Этот файл будет сгенерирован автоматически в момент сборки - см. convertIntoHeader в CMakeLists.txt:18
 #include "cl/bitonic_cl.h"
 
 #include <vector>
@@ -13,8 +12,7 @@
 
 
 template<typename T>
-void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line)
-{
+void raiseFail(const T &a, const T &b, const std::string &message, std::string filename, int line) {
     if (a != b) {
         std::cerr << message << " But " << a << " != " << b << ", " << filename << ":" << line << std::endl;
         throw std::runtime_error(message);
@@ -24,14 +22,14 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
 
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
+    // Device
     gpu::Device device = gpu::chooseGPUDevice(argc, argv);
-
     gpu::Context context;
     context.init(device.device_id_opencl);
     context.activate();
 
+    // Data
     int benchmarkingIters = 10;
     unsigned int n = 32 * 1024 * 1024;
     std::vector<float> as(n, 0);
@@ -41,6 +39,7 @@ int main(int argc, char **argv)
     }
     std::cout << "Data generated for n=" << n << "!" << std::endl;
 
+    // CPU
     std::vector<float> cpu_sorted;
     {
         timer t;
@@ -50,38 +49,54 @@ int main(int argc, char **argv)
             t.nextLap();
         }
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "CPU: " << (n/1000/1000) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "CPU: " << double(n) / 1000 / 1000 / t.lapAvg() << " millions/s" << std::endl;
     }
-/*
+
+    // GPU
     gpu::gpu_mem_32f as_gpu;
     as_gpu.resizeN(n);
-
     {
-        ocl::Kernel bitonic(bitonic_kernel, bitonic_kernel_length, "bitonic");
-        bitonic.compile();
+        ocl::Kernel bitonic_local(bitonic_kernel, bitonic_kernel_length, "bitonic_local");
+        bitonic_local.compile();
+        ocl::Kernel bitonic_initial(bitonic_kernel, bitonic_kernel_length, "bitonic_initial");
+        bitonic_initial.compile();
+
+        unsigned int wg_size = 256;
+        unsigned int gw_size = (n + wg_size - 1) / wg_size * wg_size;
+        unsigned int max_k = 2;
+        while (max_k < n) max_k *= 2;
 
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
             as_gpu.writeN(as.data(), n);
 
-            t.restart(); // Запускаем секундомер после прогрузки данных чтобы замерять время работы кернела, а не трансфер данных
+            t.restart();
 
-            unsigned int workGroupSize = 128;
-            unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
-            bitonic.exec(gpu::WorkSize(workGroupSize, global_work_size),
-                         as_gpu, n);
+            unsigned int k = 2;
+            while (k <= max_k) {
+                auto cur_k = k / 2;
+
+                while (2 * cur_k > wg_size) {
+                    bitonic_initial.exec(gpu::WorkSize(wg_size, gw_size), as_gpu, cur_k, k, n);
+                    cur_k /= 2;
+                }
+
+                bitonic_local.exec(gpu::WorkSize(wg_size, gw_size), as_gpu, cur_k, k, n);
+                k *= 2;
+            }
+
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "GPU: " << (n/1000/1000) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "GPU: " << double(n) / 1000 / 1000 / t.lapAvg() << " millions/s" << std::endl;
 
         as_gpu.readN(as.data(), n);
     }
 
-    // Проверяем корректность результатов
+    // Test
     for (int i = 0; i < n; ++i) {
         EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
+
     return 0;
 }
